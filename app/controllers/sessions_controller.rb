fdc899b08c41d9d01efe04f5c0de1fc81e063216
justus <jmbaluka@mfarm.co.ke>
2015-04-26 13:01:04 +0300

    debugged sessions_controller to allow successiful logins and logouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	   user = User.find_by_username(params[:username])
-          if user && user.authenticate(params[:password])
-            session[:userid] = user.id
-            redirect_to rooturl, notice: "Logged in!"
+  	   @user = User.find_by_username(params[:username])
+
+          if @user && @user.authenticate(params[:password])
+             session[:user_id] = @user.id
+             redirect_to @user, notice: "Logged in!"
           else
              flash[:error] = "Wrong Username or Password."
              redirect_to root_url
-           end
+          end
   end
 
   def destroy
-    session[:userid] = nil
-    redirect_to root_url, notice: "Logged out."
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out."
   end
 
 end
