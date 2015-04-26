d55aac674ad95a54fe4e5c4cad85da44c60dff6a
justus <jmbaluka@mfarm.co.ke>
2015-04-26 13:51:03 +0300

    added the ribbits controller to handle the ribbits

--- app/models/ribbit.rb
+++ app/models/ribbit.rb
@@ -1,9 +1,9 @@
 class Ribbit < ActiveRecord::Base
 
-  default_scope order: 'createdat DESC'
+  default_scope {where(order: 'createdat DESC')}
   attr_accessible :content, :userid
   belongs_to :user
  
   validates :content, length: { maximum: 140 }
-  
+
 end
