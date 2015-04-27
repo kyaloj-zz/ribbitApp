87240d3d27fa691856beec6957645f35af4e6526
justus <jmbaluka@mfarm.co.ke>
2015-04-27 07:08:58 +0300

    created user relationships models

---
+++ app/models/relationship.rb
@@ -0,0 +1,5 @@
+class Relationship < ActiveRecord::Base
+
+	belongs_to :follower, classname: "User"
+	belongs_to :followed, classname: "User"
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ActiveRecord::Base
 	has_secure_password
     has_many :ribbits
 
+    has_many :follower_relationships, classname: "Relationship", foreign_key: "follower_id"
+    has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+    
+    has_many :followers, through: :follower_relationships
+    has_many :followeds, through: :followed_relationships
 
 
 	before_validation :prep_email
@@ -13,6 +18,15 @@ class User < ActiveRecord::Base
 	validates :username, presence: true, uniqueness: true
 	validates :email, presence: true, uniqueness: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
      
+
+    def following? user
+          self.followeds.include? user
+    end
+ 
+    def follow user
+          Relationship.create follower_id: self.id, followed_id: user.id
+    end
+
      private
          def prep_email
          	self.email = self.email.strip.downcase if self.email
